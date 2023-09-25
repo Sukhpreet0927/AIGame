4c37790df6844d0389cf5841f63c9de15cd2fb92
Sukhpreet Aulakh <sukhpreetaulakh@MacBook-Air-97.local>
2023-09-24 22:09:03 -0700

    Pong training working for both paddle

---
+++ Pong.py
@@ -0,0 +1,293 @@
+import turtle
+import os
+import pygame
+import neat
+import time
+import pickle
+
+#Boundary
+Boundary = turtle.Turtle()
+Boundary.penup()
+Boundary.pensize(20)
+Boundary.speed(0)
+Boundary.goto(-450, -300)
+Boundary.color("white")
+Boundary.pendown()
+Boundary.forward(900)
+Boundary.left(90)
+Boundary.forward(600)
+Boundary.left(90)
+Boundary.forward(900)
+Boundary.left(90)
+Boundary.forward(600)
+
+class GameInformation:
+       def __init__(self, left_hits, right_hits, left_score, right_score):
+              self.left_hits = left_hits
+              self.right_hits = right_hits
+              self.left_score = left_score
+              self.right_score = right_score
+
+
+#Game itself loop
+
+class Paddle(turtle.Turtle):
+	def __init__(self, x, y, color):
+			super().__init__()
+			self.speed(0)
+			self.shape("square")
+			self.color(color)
+			self.shapesize(stretch_wid=5, stretch_len=1)
+			self.penup()
+			self.goto(x, y)
+	def __init__(self, x, y, color):
+			super().__init__()
+			self.speed(0)
+			self.shape("square")
+			self.color(color)
+			self.shapesize(stretch_wid=5, stretch_len=1)
+			self.penup()
+			self.goto(x, y)
+
+	def move_up(self):
+			y = self.ycor()
+			if y < 240:  # Adjust the limit as needed
+					y += 20
+			self.sety(y)
+	def move_up(self):
+			y = self.ycor()
+			if y < 240:  # Adjust the limit as needed
+					y += 20
+			self.sety(y)
+
+	def move_down(self):
+			y = self.ycor()
+			if y > -240:  # Adjust the limit as needed
+					y -= 20
+			self.sety(y)
+	def move_down(self):
+			y = self.ycor()
+			if y > -240:  # Adjust the limit as needed
+					y -= 20
+			self.sety(y)
+	def get_y(self):
+		return self.ycor()
+
+class Ball(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.speed(0)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.goto(0, 0)
+        self.dx = 2
+        self.dy = 2
+				
+    def move(self):
+        self.setx(self.xcor() + self.dx)
+        self.sety(self.ycor() + self.dy)
+
+# Create the screen
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Create the players
+paddle_a = Paddle(-350, 0, "blue")
+paddle_b = Paddle(350, 0, "red")
+
+# Create the ball
+ball = Ball()
+
+# Create the pen for displaying the scores
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 235)
+pen.write("Pong", align= "center", font=("Comic Sans MS", 38, "bold"))
+
+# Set initial state for ball movement
+game_started = False
+
+#Score
+score_a = 0
+score_b = 0
+
+# Function to start the game
+def start_game():
+	global game_started
+	game_started = True
+	# global game_started
+	# game_started = True
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(start_game, "space")
+wn.onkeypress(paddle_a.move_up, "w")
+wn.onkeypress(paddle_a.move_down, "s")
+wn.onkeypress(paddle_b.move_up, "Up")
+wn.onkeypress(paddle_b.move_down, "Down")
+
+# Left and right
+if ball.xcor() > 350:
+	score_a += 1
+	pen.clear()
+	pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+	ball.goto(0, 0)
+	ball.dx *= -1
+# Left and right
+if ball.xcor() > 350:
+	score_a += 1
+	pen.clear()
+	pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+	ball.goto(0, 0)
+	ball.dx *= -1
+
+elif ball.xcor() < -350:
+	score_b += 1
+	pen.clear()
+	pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+	ball.goto(0, 0)
+	ball.dx *= -1
+elif ball.xcor() < -350:
+	score_b += 1
+	pen.clear()
+	pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+	ball.goto(0, 0)
+	ball.dx *= -1
+
+# #Ball Movement (movement by x amount of pixel)
+# ball.dx = 1
+# ball.dy = 1
+def Gameloop(left_hits, right_hits, score_a, score_b):
+    #Score
+    while True:
+        wn.update()
+
+        
+        # Move the ball
+        if game_started:
+            # Move the ball
+            # ball.setx(ball.xcor() + ball.dx)
+            # ball.sety(ball.ycor() + ball.dy)
+            ball.move()
+
+        # if(wn.onkeypress("space")) :
+        #     ball.setx(ball.xcor() + ball.dx)
+        #     ball.sety(ball.ycor() + ball.dy)
+
+        # Border checking
+            # Top and bottom
+            if ball.ycor() > 290:
+                ball.sety(290)
+                ball.dy *= -1
+        
+            elif ball.ycor() < -290:
+                ball.sety(-290)
+                ball.dy *= -1
+
+            # Left and right
+            if ball.xcor() > 350:
+                score_a += 1
+                pen.clear()
+                pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                ball.goto(0, 0)
+                ball.dx *= -1
+
+            elif ball.xcor() < -350:
+                score_b += 1
+                pen.clear()
+                pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                ball.goto(0, 0)
+                ball.dx *= -1
+
+            # Paddle and ball collisions
+            if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+                left_hits += 1 
+                ball.dx *= -1
+
+                #os.system("afplay bounce.wav&")
+            
+            elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+                right_hits += 1
+                ball.dx *= -1
+                #os.system("afplay bounce.wav&")
+
+            
+
+
+            if score_a == 5:
+                ball.hideturtle()
+                paddle_a.hideturtle()
+                paddle_b.hideturtle()
+                wn.listen()
+                paddle_a.setposition(1000, 1000)
+                paddle_b.setposition(1100 , 1000)
+                win_1 = turtle.Turtle()
+                win_1.speed(0)
+                win_1.color("white")
+                win_1.penup()
+                win_1.hideturtle()
+                win_1.goto(0, 0)
+                win_1.write("Player 1 is the winner", align = "center", font = ("Impact", 28, "normal"))
+                GG_1 = turtle.Turtle()
+                GG_1.speed(0)
+                GG_1.color("yellow")
+                GG_1.penup()
+                GG_1.hideturtle()
+                GG_1.goto(0, -175)
+                GG_1.write("Good Game", align = "center", font = ("Impact", 36, "bold"))
+                game_info = GameInformation(left_hits, right_hits, score_a, score_b)
+                return game_info
+            
+
+            if score_b == 5:
+                ball.hideturtle()
+                paddle_a.hideturtle()
+                paddle_b.hideturtle()
+                wn.listen()
+                paddle_a.setposition(1000, 1000)
+                paddle_b.setposition(1100 , 1000)
+                win_2 = turtle.Turtle()
+                win_2.speed(0)
+                win_2.color("white")
+                win_2.penup()
+                win_2.hideturtle()
+                win_2.goto(0, 0)
+                win_2.write("Player 2 is the winner", align = "center", font = ("Impact", 28, "normal"))
+                GG_2 = turtle.Turtle()
+                GG_2.speed(0)
+                GG_2.color("green")
+                GG_2.penup()
+                GG_2.hideturtle()
+                GG_2.goto(0, -175)
+                GG_2.write("Good Game", align = "center", font = ("Impact", 36, "bold"))
+                game_info = GameInformation(left_hits, right_hits, score_a, score_b)
+                return game_info
+
+
+
+
+
+if __name__ == '__main__':
+        left_hits = 0
+        right_hits = 0 
+        score_a = 0 
+        score_b = 0
+        
+        game_info = Gameloop(left_hits, right_hits, score_a, score_b)
+
+
+        # Print the game information
+        print("Game Information:")
+        print(f"Left Hits: {game_info.left_hits}")
+        print(f"Right Hits: {game_info.right_hits}")
+        print(f"Left Score: {game_info.left_score}")
+        print(f"Right Score: {game_info.right_score}")
+
+        
--- test.py
+++ test.py
@@ -1,47 +1,171 @@
 #testing
 import turtle
-import pygame
-from Pong import *
+from Pong import Paddle, Ball, pen, GameInformation
 import neat
 import os
 import pickle
 
+
 class AiPongGame:
     def __init__(self):
+        self.wn = turtle.Screen()
+        self.wn.title("Pong")
+        self.wn.bgcolor("black")
+        self.wn.setup(width=800, height=600)
+        self.wn.tracer(0)
+
+
         self.left_paddle = Paddle(-350, 0, "blue")
         self.right_paddle = Paddle(350, 0, "red")
         self.ball = Ball()
 
+        self.setup_keybindings() #Call the setup_keybindings method
+
+    # Add this method to your AiPongGame class
+    def setup_keybindings(self):    
+        self.wn.listen()
+        self.wn.onkeypress(self.left_paddle.move_up, "w")
+        self.wn.onkeypress(self.left_paddle.move_down, "s")
+        self.wn.onkeypress(self.right_paddle.move_up, "Up")
+        self.wn.onkeypress(self.right_paddle.move_down, "Down")
+
+
+
+
+
+
     def test_ai(self, genome, config):
-            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            net1 = neat.nn.FeedForwardNetwork.create(genome, config)
+            score_a = 0
+            score_b = 0
+            left_hits = 0
+            right_hits = 0
             run = True
-            clock = pygame.time.Clock()
             while run:
-                wn.update()
-                clock.tick(60)
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        run = False
-                        break
-                game_info = Gameloop(0, 0, 0, 0) 
-                print(game_info.left_score, game_info.right_score)
+                self.wn.update()
+                self.ball.move()
+
+
+                # Border checking
+                # Top and bottom
+                if self.ball.ycor() > 290:
+                    self.ball.sety(290)
+                    self.ball.dy *= -1
+            
+                elif self.ball.ycor() < -290:
+                    self.ball.sety(-290)
+                    self.ball.dy *= -1
+
+                # Left and right
+                if self.ball.xcor() > 350:
+                    score_a += 1
+                    pen.clear()
+                    pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                    self.ball.goto(0, 0)
+                    self.ball.dx *= -1
+
+                elif self.ball.xcor() < -350:
+                    score_b += 1
+                    pen.clear()
+                    pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                    self.ball.goto(0, 0)
+                    self.ball.dx *= -1
+
+                # Paddle and ball collisions
+                if self.ball.xcor() < -340 and self.ball.ycor() < self.left_paddle.ycor() + 50 and self.ball.ycor() > self.left_paddle.ycor() - 50:
+                    left_hits += 1 
+                    self.ball.dx *= -1
+
+                    #os.system("afplay bounce.wav&")
+                
+                elif self.ball.xcor() > 340 and self.ball.ycor() < self.right_paddle.ycor() + 50 and self.ball.ycor() > self.right_paddle.ycor() - 50:
+                    right_hits += 1
+                    self.ball.dx *= -1
+
+
+                input2 = [self.right_paddle.get_y(), self.ball.ycor(), abs(self.right_paddle.xcor() - self.ball.xcor())]
+                output2 = net1.activate(input2)
+                decision2 = output2.index(max(output2))
+
+                
+
+
+                if decision2 == 0:
+                    pass
+                elif decision2 == 1:
+                    self.right_paddle.move_up()
+                else:
+                    self.right_paddle.move_down()
+
+                game_info = GameInformation(left_hits, right_hits, score_a, score_b)
+
+            
+                
                 
 
     def train_ai(self, genome1, genome2, config):
-            wn = turtle.Screen()
             net1 = neat.nn.FeedForwardNetwork.create(genome1, config) #neural networks
             net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
         
             run = True
+            #Score
+            score_a = 0
+            score_b = 0
+            left_hits = 0
+            right_hits = 0
+            
+
+
+
             while run:
-                wn.update()
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        quit()
+                self.wn.update()
+                self.ball.move()
                 
+
+
+
+                # Border checking
+            # Top and bottom
+                if self.ball.ycor() > 290:
+                    self.ball.sety(290)
+                    self.ball.dy *= -1
+            
+                elif self.ball.ycor() < -290:
+                    self.ball.sety(-290)
+                    self.ball.dy *= -1
+
+                # Left and right
+                if self.ball.xcor() > 350:
+                    score_a += 1
+                    pen.clear()
+                    pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                    self.ball.goto(0, 0)
+                    self.ball.dx *= -1
+
+                elif self.ball.xcor() < -350:
+                    score_b += 1
+                    pen.clear()
+                    pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                    self.ball.goto(0, 0)
+                    self.ball.dx *= -1
+
+                # Paddle and ball collisions
+                if self.ball.xcor() < -340 and self.ball.ycor() < self.left_paddle.ycor() + 50 and self.ball.ycor() > self.left_paddle.ycor() - 50:
+                    left_hits += 1 
+                    self.ball.dx *= -1
+
+                    #os.system("afplay bounce.wav&")
+                
+                elif self.ball.xcor() > 340 and self.ball.ycor() < self.right_paddle.ycor() + 50 and self.ball.ycor() > self.right_paddle.ycor() - 50:
+                    right_hits += 1
+                    self.ball.dx *= -1
+
+
+
                 # Prepare input values for both neural networks
-                input1 = [self.left_paddle.get_y(), self.ball.ycor(), abs(self.left_paddle.xcor() - ball.xcor())]
-                input2 = [self.right_paddle.get_y(), self.ball.ycor(), abs(self.right_paddle.xcor() - ball.xcor())]
+                input1 = [self.left_paddle.get_y(), self.ball.ycor(), abs(self.left_paddle.xcor() - self.ball.xcor())]
+                input2 = [self.right_paddle.get_y(), self.ball.ycor(), abs(self.right_paddle.xcor() - self.ball.xcor())]
+
     
                 # Activate the neural networks
                 output1 = net1.activate(input1)
@@ -50,11 +174,9 @@ class AiPongGame:
                 if decision1 == 0:
                     pass
                 elif decision1 == 1:
-                    self.left_paddle.hideturtle()
-                    self.left_paddle.move_up(up = True)
+                    self.left_paddle.move_up()
                 else:
-                    self.left_paddle.hideturtle()
-                    self.left_paddle.move_down(down = True)
+                    self.left_paddle.move_down()
                 
                 
                 output2 = net2.activate(input2)
@@ -66,23 +188,28 @@ class AiPongGame:
                 if decision2 == 0:
                     pass
                 elif decision2 == 1:
-                    self.right_paddle.move_up(up = True)
+                    self.right_paddle.move_up()
                 else:
-                    self.right_paddle.move_down(down = True)
+                    self.right_paddle.move_down()
 
         
     
                 print(output1, output2)
 
-                game_info = Gameloop(0,0,0,0)
+                game_info = GameInformation(left_hits, right_hits, score_a, score_b)
 
-                if game_info.left_score >= 5 or game_info.right_score >= 5 or game_info.left_hits > 5:
-                    self.calculate_fitness(genome1, genome2, game_info)
-                    self.left_paddle.hideturtle()
-                    self.right_paddle.hideturtle()
-                    self.ball.hideturtle()
+                #print(game_info.left_hits)
+                #print(game_info.right_hits)
 
-                    break
+                #game_info = Gameloop(0,0,0,0)
+
+                if game_info.left_score >= 1 or game_info.right_score >= 1 or game_info.left_score > 50:
+                     self.calculate_fitness(genome1, genome2, game_info)
+                     self.left_paddle.hideturtle()
+                     self.right_paddle.hideturtle()
+                     self.ball.hideturtle()
+
+                     break
             
     def calculate_fitness(self, genome1, genome2, game_info):
         genome1.fitness += game_info.left_hits
@@ -104,22 +231,31 @@ def eval_genomes(genomes, config):
 
 
 def run_neat(config):
+    #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-0')
     p = neat.Population(config)
+    print("fasfsadfads")
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(1)) #restart training cause it could take a while
-    winner = p.run(eval_genomes, 50)
+    
+    winner = p.run(eval_genomes, 1)
+    with open("best.pickle", "wb") as f:
+        pickle.dump(winner, f)
+
+def test_ai(config):
+    with open("best.pickle", "rb") as f:
+        winner = pickle.load(f)
+    game = AiPongGame()
+    game.test_ai(winner, config)
 
 
     
 if __name__ == "__main__":
-        pygame.init()
         local_dir = os.path.dirname(__file__)
         config_path = os.path.join(local_dir, "config.txt")
 
         config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,neat.DefaultSpeciesSet, neat.DefaultStagnation,config_path)
-        # run_neat(config)
+        
         run_neat(config)
-
-
+        #test_ai(config)
