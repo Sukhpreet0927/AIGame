a0797e6db357ded993099576b49d05d0daaac324
Sukhpreet Aulakh <sukhpreetaulakh@MacBook-Air-104.local>
2023-11-05 17:40:28 -0800

    Correctly running training

--- test.py
+++ test.py
@@ -255,6 +255,7 @@ if __name__ == "__main__":
         local_dir = os.path.dirname(__file__)
         config_path = os.path.join(local_dir, "config.txt")
 
+
         config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,neat.DefaultSpeciesSet, neat.DefaultStagnation,config_path)
         
         run_neat(config)
